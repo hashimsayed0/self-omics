58669c6ad9c1869f4a6523bd0cee7dcc46967f2f
Sayed Hashim <hashimsayed0@gmail.com>
2022-08-01 14:59:23 +0400

    edited train script

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,6 +1,6 @@
 python train.py
     --use_sample_list True --gpus -1 --split_B True 
-    --data_dir ./data/sample
+    --data_format tsv
     --split_A True --num_workers 4 --exp_name train 
     --deterministic True --mask_A True --num_mask_A 19 
     --change_ch_to_mask_every_epoch True 
